ded34d4324e5cddd5419c944b463fa22cfe5c856
keleko34 <gregory.guidero@gmail.com>
2017-04-05 23:12:31 +0200

    fix crash due to menu line

--- Screens/Main/Main.js
+++ Screens/Main/Main.js
@@ -1,7 +1,7 @@
 var base = process.cwd().replace(/\\/g,'/');
 
 /* remove base context menu */
-nw.Window.get().menu = null; //!!!not working
+//nw.Window.get().menu = null; !!!not working
 
 
 /* Globals */
